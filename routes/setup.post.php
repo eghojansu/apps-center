cd9ed101ba1c6386c58d9cddf69969ebd9749070
Eko Kurniawan <setiawanegho@gmail.com>
2022-01-18 20:29:58 +0700

    Auto import databases on setup

--- routes/setup.post.php
+++ routes/setup.post.php
@@ -5,6 +5,13 @@ use function Ekok\Cosiler\Http\Response\redirect;
 
 !is_file($versionFile = $fun['tmp_dir'] . '/version.txt') || not_found();
 
+/** @var PDO */
+$pdo = $fun['db']->getPdo();
+
+foreach (glob($fun['project_dir'] . '/databases/*.sql') as $file) {
+  $pdo->exec(file_get_contents($file));
+}
+
 $fun['db']->update('user', array('active' => 'on', 'password' => password_hash('admin123', PASSWORD_BCRYPT)), array('userid = "admin"'));
 
 file_put_contents($versionFile, 'Installed at ' . date('Y-m-d H:i:s'));
